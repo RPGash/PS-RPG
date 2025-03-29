466449ef8bbca18dc8ad4743380079a7bb9caeb4
RPG_ash <83925827+RPGash@users.noreply.github.com>
2025-03-29 20:35:41 +0000

    updated ToDo

--- PS-RPG.ps1
+++ PS-RPG.ps1
@@ -32,7 +32,6 @@
 #       crit chance,
 #       CTRL+C warning and file syncing issue (e.g. Google Drive or OneDrive etc.)
 #   - consider changing mob crit rate/damage to from fixed 20%/20% to specific % for different mobs
-#   - change leaving Home from "Leave" to "Exit"? for consistency
 #   - when viewing the available buildings in Town, none of them show which letter to choose for that selection
 #       New-Object System.Management.Automation.Host.Coordinates 0,14;$Host.UI.Write("");" "*3000
 #
